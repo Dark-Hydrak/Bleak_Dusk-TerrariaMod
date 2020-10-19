0fade14248b829a7ee25ea8b85d33e0765d6055f
WavingStory <wavingstory@outlook.fr>
2020-10-19 04:36:29 +0200

    Summon Test

---
+++ Items/EtheralGlaive.cs
@@ -0,0 +1,31 @@
+﻿using BleakDusk.Projectiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BleakDusk.Items
+{
+    class EtheralGlaive : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Made from the remains of a [c/66FFFF:frost wyvern]...");
+        }
+
+        public override void SetDefaults()
+        {
+            item.melee = true;
+            item.damage = 20;
+            item.crit = 0;
+            item.knockBack = 5;
+            item.useStyle = ItemUseStyleID.HoldingUp;
+            item.useAnimation = 300;
+            item.useTime = 10;
+            item.shoot = mod.ProjectileType("EtheralDagger");
+        }
+    }
+}
---
+++ Projectiles/EtheralDagger.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace BleakDusk.Projectiles
+{
+    class EtheralDagger : ModProjectile
+    {
+        Vector2 initialPosition = new Vector2();
+        bool setPosition = false;
+        float direction = 0;
+        // Tweaks
+        float directionIncrement = 5;
+        float rangeIncrement = 2f;
+        float range = 128;
+        float maxRange = 128;
+
+        public override void SetDefaults()
+        {
+            projectile.damage = 1;
+            projectile.penetrate = -1;
+            projectile.minion = true;
+            projectile.melee = true;
+            projectile.timeLeft = 300;
+            projectile.friendly = true;
+        }
+
+        public override void AI()
+        {
+            if (setPosition == false) { initialPosition = projectile.position; setPosition = true; }
+            projectile.position = initialPosition + new Vector2((float) Math.Cos(direction) * range, (float) Math.Sin(direction) * range);
+            projectile.rotation = direction;
+            direction+=(float) (Math.PI/180f) * directionIncrement;
+            if (Math.Abs(direction) > Math.PI*2) 
+            { 
+                direction = 0;
+                rangeIncrement *= -1;
+                directionIncrement *= -1;
+            }
+            // range += rangeIncrement;
+            /*if (Math.Abs(range) > maxRange)
+            {
+
+            }*/
+        }
+
+    }
+}
